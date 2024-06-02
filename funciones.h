27618c546c7a164ac8f2f6a948a8f9e1999b9576
jusfer24 <jusfer125@gmail.com>
2024-06-01 20:35:10 -0500

    Actualizar funciones.h

--- funciones.h
+++ funciones.h
@@ -3,4 +3,4 @@ void agregarnota(float n[][N], int, int);
 void imprimirnota(const float n[][N], int, int);
 void promedioEstudiantes(const float n[][N], int, int);
 void promedioExamen(const float n[][N], int, int, float[]);
-float MayorNotaExamen(const float n[][N], int, int);
+float MayorNotaExamen(const float [], int);
